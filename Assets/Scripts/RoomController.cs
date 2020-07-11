3237e80440df1fd86be8efb7a6b05d933ac5da94
David Bailly <david.bailly@fishingcactus.com>
2020-07-11 15:02:52 +0200

    room and whiteboard canvases

---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    public GameObject bubblesContainer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
