490e277814176baa870f039d6d4c6e5def1c5d03
So-Bou <so.bourgeau@gmail.com>
2020-07-11 13:01:22 +0200

    writed Idea class

--- Assets/Scripts/IdeasManager.cs
+++ Assets/Scripts/IdeasManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class IdeasManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        
+        //parse xml
+
+        //create idea list
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
