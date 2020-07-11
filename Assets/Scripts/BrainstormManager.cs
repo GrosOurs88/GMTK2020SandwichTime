8e27da3cf510ad170a2b6811ceea76eb4c83c066
So-Bou <so.bourgeau@gmail.com>
2020-07-11 15:06:13 +0200

    added basic timeline managing

--- Assets/Scripts/BrainstormManager.cs
+++ Assets/Scripts/BrainstormManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,20 +10,45 @@ public class BrainstormManager : MonoBehaviour
 
     private float[] timeStamps;
 
+    private float elapsedTime = 0;
+    private bool brainstormStarted = false;
+    private int currentCheckedTimeStamp = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         timeStamps = new float[bubbleAmount];
 
-        for(int i = 0; i < timeStamps.Length; i+)
+        for(int i = 0; i < timeStamps.Length; i++)
         {
-            timeStamps[i] = Random.Range(0, brainstormTime - 2); //-2 so the player has min 2 second to read the last bubble;
+            timeStamps[i] = UnityEngine.Random.Range(0, brainstormTime - 2); //-2 so the player has min 2 second to read the last bubble;
         }
+
+        Array.Sort(timeStamps);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(brainstormStarted && elapsedTime <= brainstormTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if(elapsedTime >= timeStamps[currentCheckedTimeStamp])
+            {
+                //request bubble display
+                Debug.Log("display bubble");
+
+                if(currentCheckedTimeStamp < timeStamps.Length - 1)
+                    currentCheckedTimeStamp++ ;
+            }
+        }
+    }
+
+    void startBrainstorm()
+    {
+        brainstormStarted = true;
+        elapsedTime = 0;
     }
 }
