710109f50fe77df368d085bf69604f0558baa91f
Hugo Bourbon <hugo.bourbon@fishingcactus.com>
2020-07-11 18:48:58 +0200

    Add a master for buttons (actually used for scenes transitions)

---
+++ Assets/Scripts/MasterButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterButtonScript : MonoBehaviour
+{
+    public Canvas canvasMainMenu;
+    public Canvas canvasObjectifs;
+
+
+    //Singleton
+    public static MasterButtonScript instance;
+
+    void Awake()
+    {
+        //***SINGLETON***
+        instance = this;
+    }
+
+    public void TransitionToObjectivesCanvas()
+    {
+
+    }
+
+
+
+
+
+}
