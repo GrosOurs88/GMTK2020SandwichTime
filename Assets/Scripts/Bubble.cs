3a54fdf10d8376c2d379f435c3aa525e480ae40e
David Bailly <david.bailly@fishingcactus.com>
2020-07-11 16:26:29 +0200

    Bubble script and reference to bubble prefab in RoomController

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Bubble : MonoBehaviour
+{
+    public TMP_Text text;
+    public Image characterImage;
+    public Image bubbleBackground;
+    public CanvasGroup canvasGroup;
+
+    public void Kill()
+    {
+        //TODO coroutine alpha to 0 then destroy
+    }
+}
