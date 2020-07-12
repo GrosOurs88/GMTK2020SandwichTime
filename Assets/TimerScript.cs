ce210daf4cc25a24a9fc4b62afd414a80736d81e
Hugo Bourbon <hugo.bourbon@fishingcactus.com>
2020-07-12 16:48:59 +0200

    Add timer script

---
+++ Assets/TimerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    public float timer;
+    public TextMeshProUGUI timerUI;
+    private bool meetingAlmostEnded = false;
+
+ 
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+        }            
+
+        if(timer <= 0f)
+        {
+            MasterSoundsScript.instance.PlayMeetingEnd();
+            timer = 0f;
+        }
+
+        if(timer <= 10f && meetingAlmostEnded == false)
+        {
+            meetingAlmostEnded = true;
+
+            MasterSoundsScript.instance.PlayMeetingAboutToEnd();
+        }
+
+        float minutes = (int)(timer / 60);
+        float seconds = (int)(timer % 60);
+
+        timerUI.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
+}
