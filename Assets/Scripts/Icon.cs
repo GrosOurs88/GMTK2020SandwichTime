a03f0dd1c66ad53225a9f73ecbb53dcb78d110b4
David Bailly <david.bailly@fishingcactus.com>
2020-07-12 12:04:43 +0200

    Icons

---
+++ Assets/Scripts/Icon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Icon : MonoBehaviour
+{
+    public float timeToLive = 1f;
+    public Image image;
+    public CanvasGroup canvasGroup;
+
+    private float timeAlive;
+
+    void Update()
+    {
+        timeAlive += Time.deltaTime;
+
+        canvasGroup.alpha = 1- (timeAlive / timeToLive); 
+
+        if (timeAlive >= timeToLive)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/IconsSpawner.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconsSpawner : MonoBehaviour
+{
+    public GameObject iconPrefab;
+    public float iconSpawnDelay = 0.2f;
+
+    private float timeSinceLastSpawn;
+    private List<Sprite> IconToSpawnQueue;
+
+    private void Update()
+    {
+        if (IconToSpawnQueue.Count > 0)
+        {
+            timeSinceLastSpawn += Time.deltaTime;
+
+            if (timeSinceLastSpawn > iconSpawnDelay)
+            {
+                timeSinceLastSpawn -= iconSpawnDelay;
+
+                SpawnIcon(IconToSpawnQueue[0]);
+                IconToSpawnQueue.RemoveAt(0);
+
+                if (IconToSpawnQueue.Count == 0)
+                {
+                    timeSinceLastSpawn = iconSpawnDelay;
+                }
+            }
+        }
+    }
+    
+    private void SpawnIcon(Sprite sprite)
+    {
+        Vector3 pos = new Vector3(Random.Range(-50f, 50f), 0f, 0f);
+        Instantiate(iconPrefab, pos, Quaternion.identity, transform).GetComponent<Icon>().image.sprite = sprite;
+    }
+
+    public void QueueIconSpawn(Theme theme)
+    {
+        Sprite sprite = Resources.Load<Sprite>("Icons/" + theme.ToString());
+        IconToSpawnQueue.Add(sprite);
+    }
+    public void QueueIconSpawn(IconType type, int amount)
+    {
+        Sprite sprite =null;
+
+        switch (type)
+        {
+            case IconType.Budget:
+                {
+                    if (amount > 0)
+                    {
+                        sprite = Resources.Load<Sprite>("Icons/Budget");
+                    }
+                    else
+                    {
+                        sprite = Resources.Load<Sprite>("Icons/BudgetDown");
+                    }
+
+                    break;
+                }
+            case IconType.Appeal:
+                {
+                    if (amount > 0)
+                    {
+                        sprite = Resources.Load<Sprite>("Icons/Appeal");
+                    }
+                    else
+                    {
+                        sprite = Resources.Load<Sprite>("Icons/AppealDown");
+                    }
+
+                    break;
+                }
+            case IconType.Audience:
+                {
+                    sprite = Resources.Load<Sprite>("Icons/Audience_" + amount.ToString());
+
+                    break;
+                }
+        }
+        
+        for (int i = amount; i>0; i--)
+        {
+            IconToSpawnQueue.Add(sprite);
+        }
+    }
+
+}
+
+public enum IconType
+{
+    Theme,
+    Budget,
+    Appeal,
+    Audience
+}
